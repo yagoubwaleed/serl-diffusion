09d096c6e96750ed34d3d5f0f6765830f62f7c3d
JacobB33 <hockeyviolin@gmail.com>
2024-07-09 16:51:08 -0700

    These are the GOATED params

--- serl_robot_infra/franka_env/envs/pushing_env/config.py
+++ serl_robot_infra/franka_env/envs/pushing_env/config.py
@@ -15,30 +15,30 @@ class PushingEnvConfig(DefaultEnvConfig):
         0.01697357310341893,
         0.030485965848999902,
         3.1130148, 0.0956483, -0.0854985])
-    RESET_POSE = TARGET_POSE + np.array([-0.3, 0.0, 0.1, 0.0, 0.0, 0.0])
-    REWARD_THRESHOLD: np.ndarray = np.array([0.02, 0.02, 0.02, 0.2, 0.2, 0.2])
-    ACTION_SCALE = np.array([0.3, 0.3, 0.8])
+    RESET_POSE = TARGET_POSE + np.array([-0.06, 0.0, 0.0, 0.0, 0.0, 0.0])
+    REWARD_THRESHOLD: np.ndarray = np.array([0.001, 0.0001, 0.0001, 0.0001, 0.0001, 0.0001])
+    ACTION_SCALE = np.array([0.1, 0.1, 1])
     RANDOM_RESET = False
-    RANDOM_XY_RANGE = 0.4
-    RANDOM_RZ_RANGE = np.pi / 7
+    RANDOM_XY_RANGE = 0.1
+    RANDOM_RZ_RANGE = np.pi / 6
     ABS_POSE_LIMIT_LOW = np.array(
         [
-            TARGET_POSE[0] - RANDOM_XY_RANGE,
-            TARGET_POSE[1] - RANDOM_XY_RANGE,
-            TARGET_POSE[2],
-            TARGET_POSE[3] - 0.03,
-            TARGET_POSE[4] - 0.03,
-            TARGET_POSE[5] - RANDOM_RZ_RANGE,
+            TARGET_POSE[0] - 1,
+            TARGET_POSE[1] - 1,
+            TARGET_POSE[2] - 1,
+            TARGET_POSE[3] - np.pi / 2,
+            TARGET_POSE[4] - np.pi / 2,
+            TARGET_POSE[5] - np.pi / 2,
         ]
     )
     ABS_POSE_LIMIT_HIGH = np.array(
         [
-            TARGET_POSE[0] + RANDOM_XY_RANGE,
-            TARGET_POSE[1] + RANDOM_XY_RANGE,
-            TARGET_POSE[2] + 0.1,
-            TARGET_POSE[3] + 0.03,
-            TARGET_POSE[4] + 0.03,
-            TARGET_POSE[5] + RANDOM_RZ_RANGE,
+            TARGET_POSE[0] + 1,
+            TARGET_POSE[1] + 1,
+            TARGET_POSE[2] + 1,
+            TARGET_POSE[3] + np.pi / 2,
+            TARGET_POSE[4] + np.pi / 2,
+            TARGET_POSE[5] + np.pi / 2,
         ]
     )
     COMPLIANCE_PARAM = {
@@ -47,17 +47,17 @@ class PushingEnvConfig(DefaultEnvConfig):
         "rotational_stiffness": 150,
         "rotational_damping": 7,
         "translational_Ki": 0,
-        "translational_clip_x": 0.003,
-        "translational_clip_y": 0.003,
+        "translational_clip_x": 0.006,
+        "translational_clip_y": 0.006,
         "translational_clip_z": 0.005,
-        "translational_clip_neg_x": 0.003,
-        "translational_clip_neg_y": 0.003,
+        "translational_clip_neg_x": 0.006,
+        "translational_clip_neg_y": 0.006,
         "translational_clip_neg_z": 0.005,
-        "rotational_clip_x": 0.02,
-        "rotational_clip_y": 0.02,
+        "rotational_clip_x": 0.05,
+        "rotational_clip_y": 0.05,
         "rotational_clip_z": 0.02,
-        "rotational_clip_neg_x": 0.02,
-        "rotational_clip_neg_y": 0.02,
+        "rotational_clip_neg_x": 0.05,
+        "rotational_clip_neg_y": 0.05,
         "rotational_clip_neg_z": 0.02,
         "rotational_Ki": 0,
     }
@@ -81,3 +81,4 @@ class PushingEnvConfig(DefaultEnvConfig):
         "rotational_clip_neg_z": 0.05,
         "rotational_Ki": 0.1,
     }
+    MAX_EPISODE_STEPS = 100000
